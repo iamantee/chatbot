73436317dea92b617429d031c85fc218de960e6e
antee <iamantee@gmail.com>
2018-11-06 12:58:17 +0800

    Update http.cpp

--- modules/http/http.cpp
+++ modules/http/http.cpp
@@ -17,8 +17,6 @@ typedef ssl::stream<tcp::socket> ssl_socket;
 class HTTPUtil 
 {
     private: 
-        // BOOST_ASIO_STRING_VIEW_PARAM _host;
-        // BOOST_ASIO_STRING_VIEW_PARAM _port;
         std::string _host;
         std::string _port;
         boost::string_view _target;
@@ -83,21 +81,6 @@ class HTTPUtil
                 sock.set_verify_callback(ssl::rfc2818_verification(_host));
                 sock.handshake(ssl_socket::client);
 
-                /*
-                // The io_context is required for all I/O
-                boost::asio::io_context ioc;
-
-                // These objects perform our I/O
-                tcp::resolver resolver{ioc};
-                tcp::socket socket{ioc};
-
-                // Look up the domain name
-                auto const results = resolver.resolve(_host, _port);
-
-                // Make the connection on the IP address we get from a lookup
-                boost::asio::connect(socket, results.begin(), results.end());
-                */
-
                 // Set up an HTTP GET request message
                 http::request<http::string_body> req{http::verb::get, _target, _version};
                 req.set(http::field::host, _host);
@@ -125,7 +108,6 @@ class HTTPUtil
 
                 // not_connected happens sometimes
                 // so don't bother reporting it.
-                //
                 if(ec && ec != boost::system::errc::not_connected)
                     throw boost::system::system_error{ec};
 
@@ -161,77 +143,3 @@ int main(int argc, char** argv)
     http.get();
 
 }
-
-/*
-int main(int argc, char** argv)
-{
-    try
-    {
-        // Check command line arguments.
-        if(argc != 4 && argc != 5)
-        {
-            std::cerr <<
-                "Usage: http-client-sync <host> <port> <target> [<HTTP version: 1.0 or 1.1(default)>]\n" <<
-                "Example:\n" <<
-                "    http-client-sync www.example.com 80 /\n" <<
-                "    http-client-sync www.example.com 80 / 1.0\n";
-            return EXIT_FAILURE;
-        }
-        auto const host = argv[1];
-        auto const port = argv[2];
-        auto const target = argv[3];
-        int version = argc == 5 && !std::strcmp("1.0", argv[4]) ? 10 : 11;
-
-        // The io_context is required for all I/O
-        boost::asio::io_context ioc;
-
-        // These objects perform our I/O
-        tcp::resolver resolver{ioc};
-        tcp::socket socket{ioc};
-
-        // Look up the domain name
-        auto const results = resolver.resolve(host, port);
-
-        // Make the connection on the IP address we get from a lookup
-        boost::asio::connect(socket, results.begin(), results.end());
-
-        // Set up an HTTP GET request message
-        http::request<http::string_body> req{http::verb::get, target, version};
-        req.set(http::field::host, host);
-        req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
-
-        // Send the HTTP request to the remote host
-        http::write(socket, req);
-
-        // This buffer is used for reading and must be persisted
-        boost::beast::flat_buffer buffer;
-
-        // Declare a container to hold the response
-        http::response<http::dynamic_body> res;
-
-        // Receive the HTTP response
-        http::read(socket, buffer, res);
-
-        // Write the message to standard out
-        std::cout << res << std::endl;
-
-        // Gracefully close the socket
-        boost::system::error_code ec;
-        socket.shutdown(tcp::socket::shutdown_both, ec);
-
-        // not_connected happens sometimes
-        // so don't bother reporting it.
-        //
-        if(ec && ec != boost::system::errc::not_connected)
-            throw boost::system::system_error{ec};
-
-        // If we get here then the connection is closed gracefully
-    }
-    catch(std::exception const& e)
-    {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
-    return EXIT_SUCCESS;
-}
-*/
